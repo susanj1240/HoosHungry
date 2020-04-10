301e980896c3bb1055c0cfd08d1b58bdfd23a154
ezl9uu <43938911+ezl9uu@users.noreply.github.com>
2020-04-10 18:31:30 -0400

    Update loginInfo.sql

--- sql/loginInfo.sql
+++ sql/loginInfo.sql
@@ -10,3 +10,22 @@ CREATE TABLE `loginInfo` (
 
 ALTER TABLE `loginInfo`
     ADD PRIMARY KEY (`email`);
+
+CREATE TABLE `reviewInfo` (
+    `reviewID` int NOT NULL AUTO_INCREMENT, 
+    `userText` text NOT NULL,
+    `numStars` int NOT NULL
+);
+
+ALTER TABLE `reviewInfo` (
+    ADD PRIMARY KEY (`reviewID`);
+);
+
+CREATE TABLE `favs` (
+    `email` varchar(255) NOT NULL,
+    `restaurant` varchar(255) NOT NULL
+);
+
+ALTER TABLE `favs` (
+    ADD PRIMARY KEY (`email`, `restaurant`);
+);

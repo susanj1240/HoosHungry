4d31e49cceded7cd079f1d92663ba9a5ddd15cec
ezl9uu <43938911+ezl9uu@users.noreply.github.com>
2020-04-27 18:53:25 -0400

    Delete user.ts

--- app /user.ts
+++
@@ -1,9 +0,0 @@
-// Author: Emily Lin
-export class User {
-    // Reference: https://angular.io/guide/forms-overview
-    constructor(
-        public signUpEmail: string,
-        public signUpPassword: string,
-        public confirmPassword: string
-    ) { }
-}
